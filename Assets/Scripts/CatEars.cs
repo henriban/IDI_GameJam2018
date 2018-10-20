7ec72eccbb6907f2e2c032f83f9227a2103e95af
camorte <carloalfredo.morte@gmail.com>
2018-10-20 23:12:28 +0200

    add cat ears and double jump functionality

---
+++ Assets/Scripts/CatEars.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatEars : MonoBehaviour, Hat_Interface {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public string getName()
+    {
+        return "Catears";
+    }
+
+    public void onEquip()
+    {
+        Player.player.setDoubleJump(true);
+    }
+
+    public void unequip()
+    {
+        Player.player.setDoubleJump(false);
+    }
+
+}
