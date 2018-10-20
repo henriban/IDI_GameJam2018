7bed4407c6acbb7c6715d08ca3e8b7cc040e8449
camorte <carloalfredo.morte@gmail.com>
2018-10-20 23:26:06 +0200

    fix names of script files

--- Assets/Scripts/PropelHat.cs
+++ Assets/Scripts/PropelHat.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatEars : MonoBehaviour, Hat_Interface {
+public class PropelHat : MonoBehaviour, Hat_Interface {
 
 	// Use this for initialization
 	void Start () {
