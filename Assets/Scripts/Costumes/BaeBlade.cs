c2c38e74bd2e3b848fd1ad871817d4b58c908f09
unknown <erlendgje@gmail.com>
2018-10-21 12:20:54 +0200

    Baeblade

--- Assets/Scripts/Costumes/BaeBlade.cs
+++ Assets/Scripts/Costumes/BaeBlade.cs
@@ -3,29 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaeBlade : MonoBehaviour, Costume_Interface {
+
+	private int damage = 100;
+	private string name = "Baeblade";
+	private float force = 13;
+	private float attackWidth = 0.5f;
+	private float time = 1f;
+	private float timer = 1f;
+
 	public int getDamage() {
-		throw new System.NotImplementedException();
+		return damage;
 	}
 
 	public string getName() {
-		throw new System.NotImplementedException();
-	}
-
-	public void onEquip(Player p) {
-		throw new System.NotImplementedException();
+		return name;
 	}
 
 	public void onSpecial(Player p) {
-		throw new System.NotImplementedException();
+		if(timer == time) {
+			Rigidbody2D r2d = p.GetComponent<Rigidbody2D>();
+			r2d.AddForce(new Vector2(p.getDirection() * force, 10), ForceMode2D.Impulse);
+			StartCoroutine(youSpinMeRightRounBBY(p));
+		}
 	}
 
-	// Use this for initialization
-	void Start () {
-		
+	private IEnumerator youSpinMeRightRounBBY(Player p) {
+
+		while(timer > 0) {
+			p.attack(attackWidth);
+			timer -= Time.deltaTime;
+			yield return new WaitForSeconds(0);
+		}
+
+		timer = time;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
