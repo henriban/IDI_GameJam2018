c90665c29b1fad895027dd0143ec8aaabdb3da32
Henrik Bjelke Anderson <henriban@stud.ntnu.no>
2018-10-21 14:25:33 +0200

    Can move, dmg from burning tree

---
+++ Assets/Scripts/BurningTreeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurningTreeScript : Character {
+
+    public PolygonCollider2D treeCollider;
+    public GameObject firePolygon;
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.tag == "Player") {
+            collision.gameObject.GetComponent<Player>().takeDamage(getAttackDamage(), -1, 20f);
+
+            extinguishBurningTree();
+        }
+    }
+
+    public void extinguishBurningTree() {
+        treeCollider.enabled = false;
+        firePolygon.GetComponent<FireParticleEffect>().enabled = false;
+    }
+}
