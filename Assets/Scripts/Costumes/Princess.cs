d8c8a249b68d892b58f03f3edf8d18a815eb7cd9
camorte <carloalfredo.morte@gmail.com>
2018-10-21 16:25:28 +0200

    add princess costume class and new costumeChange function

---
+++ Assets/Scripts/Costumes/Princess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Princess : MonoBehaviour, Costume_Interface {
+
+    public int getDamage()
+    {
+        return 0;
+    }
+
+    public string getName()
+    {
+        return "princess";
+    }
+
+    public void onSpecial(Player p)
+    {
+        p.costumeTransform("Princess", "princess_frog");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
