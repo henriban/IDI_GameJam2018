5ac2f433b7ab3c75650b2ac4e78ed9fb4debdc9a
Henrik Bjelke Anderson <henriban@stud.ntnu.no>
2018-10-21 16:05:46 +0200

    Lava!

---
+++ Assets/Scripts/Costumes/FireFighter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFighter : MonoBehaviour, Costume_Interface {
+
+    private int damage = 20;
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public string getName() {
+        return "firefighter";
+    }
+
+    public void onSpecial(Player p) {
+        p.attack(10.0f);
+    }
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
