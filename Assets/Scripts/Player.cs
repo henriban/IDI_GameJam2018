a308da0a6422a6e9bc1cb95bc78f0650794b1d39
unknown <erlendgje@gmail.com>
2018-10-21 14:51:56 +0200

    Endret karakteren til å kunne BAEBLADE

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -56,10 +56,6 @@ public class Player : Character {
         activeHat = 0;
         activeCostume = 0;
         numberOfJumps = 0;
-
-        hats.Add(new Propeller());
-        costumes.Add(new BaeBlade());
-        costumes.Add(new Fighter());
 	}
 
 	private void Update() {
